5247d4571428975bb5613d26f8bcd337dcaf2a0d
David Michael II <knowdavidmichael@gmail.com>
2022-10-05 18:33:33 +0100

    fix: errors

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -15,6 +15,9 @@ module.exports = ({ env }) => ({
       ssl: {
         rejectUnauthorized: false
       },
+      settings: {
+        forceMigration: false
+      }
     },
     debug: false,
   },
--- src/api/product/routes/product.js
+++ src/api/product/routes/product.js
@@ -7,3 +7,18 @@
 const { createCoreRouter } = require('@strapi/strapi').factories;
 
 module.exports = createCoreRouter('api::product.product');
+
+
+// for custom.js
+// module.exports = {
+//   routes: [
+//     {
+//       method: 'GET',
+//       path: '/products/:slug',
+//       handler: 'product.findOne',
+//       config: {
+//         auth: false,
+//       }
+//     }
+//   ]
+// }
